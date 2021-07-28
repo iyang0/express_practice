99eb30f1b18e53b06879db3912fa34acee8db651
Mike Chang <mchang95@gmail.com>
2021-07-28 15:53:24 -0700

    adds median, mode, and middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 const express = require("express");
 const app = express();
+const stats = require("./stats.js")
 
 // useful error class to throw
 const { NotFoundError, BadRequestError } = require("./expressError");
@@ -10,29 +11,60 @@ const { NotFoundError, BadRequestError } = require("./expressError");
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
+/** Validates numbers and saves as an array to request object */
+function validateNums(req, res, next) {
+  //optional chaining
+  let values = req.query.nums?.split(",").map(e => Number(e));
+
+  if (!values) {
+    throw new BadRequestError("nums are required");
+  }
+
+  if (values.some(e => isNaN(e))) {
+    throw new BadRequestError("invalid number recieved");
+  }
+
+  req.values = values;
+  next();
+}
+
+
+/** Calls validateNums middleware before every request */
+app.use(validateNums);
+
+
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
-app.get("/mean", function(req, res){
-    
-    console.log(req.query)
-    //optional chaining
-    let values = req.query.nums?.split(",").map( e => Number(e));
-    if(!values || values.some( e => isNaN(e))){
-        throw new BadRequestError();
-    }
-    
-    let mean = findMean(nums);
-    
-    return res.json({
-        operation: "mean",
-        value: mean,
-    });
+app.get("/mean", function (req, res) {
+
+  let mean = stats.findMean(req.values);
+
+  return res.json({
+    operation: "mean",
+    value: mean,
+  });
 });
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res) {
 
+  let median = stats.findMedian(req.values);
+
+  return res.json({
+    operation: "median",
+    value: median,
+  });
+});
 
 /** Finds mode of nums in qs: returns {operation: "mode", result } */
+app.get("/mode", function (req, res) {
 
+  let mode = stats.findMode(req.values);
+
+  return res.json({
+    operation: "mode",
+    value: mode,
+  });
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
